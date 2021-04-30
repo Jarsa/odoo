eef6e12aa8b1286ae9345e20d1363f8ba42120b7
jesus01x <xzan12@outlook.com>
2021-04-30 08:32:58 -0500

    [FIX] odoo: remove domain with non-exist field

--- addons/account/models/chart_template.py
+++ addons/account/models/chart_template.py
@@ -825,12 +825,10 @@ class AccountTaxTemplate(models.Model):
     cash_basis_account_id = fields.Many2one(
         'account.account.template',
         string='Tax Received Account',
-        domain=[('deprecated', '=', False)],
         oldname='cash_basis_account',
         help='Account used as counterpart for the journal entry, for taxes eligible based on payments.')
     cash_basis_base_account_id = fields.Many2one(
         'account.account.template',
-        domain=[('deprecated', '=', False)],
         string='Base Tax Received Account',
         help='Account that will be set on lines created in cash basis journal entry and used to keep track of the tax base amount.')
 
