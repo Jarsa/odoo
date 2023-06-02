c830f1d64848f4aa545a98d233783e13593e4504
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2023-06-02 23:09:47 +0000

    [REM] end-migration: remove wrong code

--- odoo/addons/base/migrations/1.4/end-migration.py
+++ odoo/addons/base/migrations/1.4/end-migration.py
@@ -361,20 +361,6 @@ def adapt_edi_format_to_mx(env):
         "edi_format_ids": [(6, 0, env.ref("l10n_mx_edi.edi_cfdi_3_3").ids"))],
     })
 
-
-def unreconcile_statement_lines_with_no_reconciliation(env):
-    _logger.warning('Unreconcile statement lines with no reconciliation')
-    statements = env["account.bank.statement"].search([
-        ("state", "=", "open"),
-    ])
-    for statement in statements:
-        statement.button_post()
-        for line in statement.line_ids:
-            not_reconciled = line.move_id.line_ids.filtered(lambda l: not l.reconciled and l.account_id.reconcile or l.account_id.id in [8279, 8234, 8224, 8229])
-            if not_reconciled:
-                line.button_undo_reconciliation()
-
-
 @openupgrade.migrate()
 def migrate(env, installed_version):
     _logger.warning('Delete records from XML ID')
@@ -441,7 +427,6 @@ def migrate(env, installed_version):
     set_payment_accounts(env)
     copy_xml_from_payment_to_move(env)
     adapt_edi_format_to_mx(env)
-    unreconcile_statement_lines_with_no_reconciliation(env)
     _logger.warning("Delete ir.ui.view.custom")
     env.cr.execute("DELETE FROM ir_ui_view_custom;")
     _logger.warning("Restore module base to version 1.4")
